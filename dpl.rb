7f6a2d856660dd2ea5efbaa82d58768599f46eb5
shu-pf <s_furuse@yumemi.co.jp>
2023-12-28 01:22:29 +0900

    Add Dpl formula for Homebrew

---
+++ dpl.rb
@@ -0,0 +1,16 @@
+# Documentation: https://docs.brew.sh/Formula-Cookbook
+#                https://rubydoc.brew.sh/Formula
+# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+class Dpl < Formula
+  desc "Command line tool for deepl translation."
+  homepage ""
+  url "https://github.com/shu-pf/dpl/releases/download/v0.0.15/dpl-macos-arm64"
+  sha256 "024d4f10140c78bb8babe5428199aba0e532a262a4776d5b41fd58bf6f6218ee"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    bin.install "dpl-macos-arm64" => "dpl"
+  end
+end
