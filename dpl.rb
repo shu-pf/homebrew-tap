307bdfae150af5b39177692a3e7c4d20b24285c0
shu-pf <s_furuse@yumemi.co.jp>
2023-12-28 12:40:16 +0900

    Update dpl.rb with new sha256 hash

--- dpl.rb
+++ dpl.rb
@@ -5,16 +5,12 @@ class Dpl < Formula
   desc "Command line tool for deepl translation."
   homepage ""
   url "https://github.com/shu-pf/dpl/releases/download/v1.0.0/dpl-macos-arm64"
-  sha256 "024d4f10140c78bb8babe5428199aba0e532a262a4776d5b41fd58bf6f6218ee"
+  sha256 "74ef06b9d87f290fbc3ee436e46096bf35da8ee5a61c86d79948b36d6ec49d6b"
   license ""
 
   # depends_on "cmake" => :build
 
   def install
-    bin.install "dpl-macos-arm64" => "dpl"
-  end
-
-  def upgrade
     bin.install "dpl-macos-arm64" => "dpl"
   end
 end
