bd8161914b4992785c05f8f5857ba89592bf57da
shu-pf <s_furuse@yumemi.co.jp>
2023-12-28 13:03:40 +0900

    Update sha256 checksum in dpl.rb

--- dpl.rb
+++ dpl.rb
@@ -5,7 +5,7 @@ class Dpl < Formula
   desc "Command line tool for deepl translation."
   homepage ""
   url "https://github.com/shu-pf/dpl/releases/download/v1.0.1/dpl-macos-arm64"
-  sha256 "74ef06b9d87f290fbc3ee436e46096bf35da8ee5a61c86d79948b36d6ec49d6b"
+  sha256 "585e190fb80135fcf2abc82d44ff28d7f94c6d2f2eec3df4b46def5cb5ea8063"
   license ""
 
   # depends_on "cmake" => :build
